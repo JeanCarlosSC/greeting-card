03de750ff4e6db83e651c216ea0a446fa54d8355
JeanCarlosSC <jeancarlosodi@gmail.com>
2023-09-21 07:08:47 -0500

    updating cata module

---
+++ social/people/Person.kt
@@ -0,0 +1,17 @@
+class Person (val name: String) {
+    fun addOccupation(occupation: String) {
+
+    }
+    fun addOccupation(occupation: String, place: String) {
+
+    }
+    fun setInterests(listOf: List<String>) {
+
+    }
+    fun setPossibleInterests(listOf: List<String>) {
+
+    }
+    fun write(msg: String) {
+        
+    }
+}
